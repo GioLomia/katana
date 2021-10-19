ef05deb6d059df45df2320e648eccb8d4d10b65c
Giorgi Lomia <lomiag@bereaalumni.org>
2021-10-19 20:22:29 +0000

    Reaching the logs but lacking permissions.

--- scripts/git_ci_stats.py
+++ scripts/git_ci_stats.py
@@ -9,9 +9,9 @@ import tempfile
 from pathlib import Path
 import github
 
-
 import requests
 from requests.auth import HTTPBasicAuth
+import time
 
 
 def get_master(repo):
@@ -48,33 +48,57 @@ def unpack_commits(commits):
         commits_ls.append(commits[i])
     return commits_ls
 
-def unpack_check_suite(suite):
-    for run in suite:
-        print("         ", run.name)
+def unpack_wf_runs(wf_runs):
+    all_runs = {"MostRecentWorkflow" : wf_runs[0].id}
+    i = 0
 
+    for run in wf_runs:
+        run_json = {}
+        run_json["event"] = run.event
+        run_json["head_branch"] = run.head_branch
+        run_json["head_sha"] = run.head_sha
+        response = requests.get(run.jobs_url).json()
+        run_json["jobs"] = response
+        print(run.jobs_url)
+        all_runs[run.id] = run_json
+        i+=1
+        if i == 10:
+            break
+
+    return all_runs
+
+def unpack_workflows(workflows):
+    all_wf = {}
+    for wf in workflows:
+        print(wf.url)
+        wf_runs = wf.get_runs()
+        all_wf[wf.id] = unpack_wf_runs(wf_runs)
+    return all_wf
+
+def save_data(path, output_json):
+    with open(f"{path}/workflows.json", "w") as output_file:
+        json.dump(output_json, output_file, indent=4)
+
+def load_data(path):
+    return json.load(open(f"{path}/workflows.json"))
 
 def main(args):
     g = github.Github(os.environ["GITHUB_TOKEN"])
     repo = g.get_repo(args.url)
     print(repo)
-    commits = repo.get_commits()
-    commits_ls = unpack_commits(commits)
-    my_commit = commits_ls[1]
-    commit_check_suite = my_commit.get_check_suites()
-    commit_run_check = my_commit.get_check_runs()
-    print(my_commit, commit_run_check)
-    
-    for suite in commit_check_suite:
-        unpack_check_suite(suite.get_check_runs())
 
-    # for check in commit_run_check:
-    #     print(check.name)
-    #     annotations = check.get_annotations()
-    #     # unpack_check_run(annotations)
-    #     print()
+    data_path = f"{args.result_dir}/"
+    if not args.load:
+        print("Getting Data from GitHub...")
+        workflows = repo.get_workflows()
+        output_json = unpack_workflows(workflows)
+        save_data(data_path,output_json)
+        data = load_data(data_path)
+    else:
+        print(f"Loading Data from saved json at {data_path}")
+        data = load_data(data_path)
 
-        # unpack_check_suite(check)
-
+    # print(data)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -82,7 +106,14 @@ if __name__ == "__main__":
         "--url", help="Repo to query file changes.",
     )
     parser.add_argument(
-        "--result-dir", help="Directory to output file change information.",
+        "--result-dir", default=None, help = "Directory to output file change information.",
     )
+    parser.add_argument(
+        "--load", dest="load", action="store_true", help = "Determines if we should load the data from disc or collect it through the GitHub API"
+    )
+    parser.add_argument(
+        "--no-load", default="load", action="store_true", help = "Determines if we should load the data from disc or collect it through the GitHub API"
+    )
+    parser.set_defaults(load = False)
     args = parser.parse_args()
     main(args)
